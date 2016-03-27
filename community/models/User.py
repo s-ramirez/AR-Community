f39631cb5c585b1536742ec3022722843b696a99
Melisa Carranza <melisacz@gmail.com>
2016-03-27 19:07:35 -0400

    fixing more indentation stuff

--- community/models/User.py
+++ community/models/User.py
@@ -1,14 +1,16 @@
 from community.core import db
 
+groups = db.Table('group_user', db.Column('group', db.Integer, db.ForeignKey('group.id')), db.Column('user', db.Integer, db.ForeignKey('user.id')))
+
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(32), unique = True)
-    name = db.Column(db.String(32))
-    groups = db.relationship('Group', backref='users', lazy='dynamic')
+	id = db.Column(db.Integer, primary_key = True)
+	username = db.Column(db.String(32), unique = True)
+	name = db.Column(db.String(32))
+	groups = db.relationship('GroupUser', secondary=groups, backref=db.backref('group_user', lazy='dynamic'))
 
-    def __init__(self, username, name):
-        self.username = username
-        self.name = name
+	def __init__(self, username, name):
+		self.username = username
+		self.name = name
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+	def __repr__(self):
+		return '<User %r>' % self.username
