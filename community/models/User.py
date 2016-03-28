b4e68663ce723d86a701a5a3d27b2349021413cc
Melisa Carranza <melisacz@gmail.com>
2016-03-28 01:17:28 -0400

    creating friendship relationship

--- community/models/User.py
+++ community/models/User.py
@@ -1,12 +1,14 @@
 from community.core import db
 from community.models.Group import Group
 groups = db.Table('group_user', db.Column('group', db.Integer, db.ForeignKey('group.id')), db.Column('user', db.Integer, db.ForeignKey('user.id')))
+friends = db.Table('user_friend', db.Column('user', db.Integer, db.ForeignKey('user.id')), db.Column('user', db.Integer, db.ForeignKey('user.id')))
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	username = db.Column(db.String(32), unique = True)
 	name = db.Column(db.String(32))
 	groups = db.relationship(Group, secondary=groups, backref=db.backref('group_user', lazy='dynamic'))
+	friends = db.relationship('User', secondary=friends, backref=db.backref('user_friend', lazy='dynamic'))
 
 	def __init__(self, username, name):
 		self.username = username
