f4557717ed81fefc74779d86d1303c10f5e9269d
Melisa Carranza <melisacz@gmail.com>
2016-03-28 09:29:33 -0400

    friends relationship fixed

--- community/models/User.py
+++ community/models/User.py
@@ -1,7 +1,7 @@
 from community.core import db
 from community.models.Group import Group
 groups = db.Table('group_user', db.Column('group', db.Integer, db.ForeignKey('group.id')), db.Column('user', db.Integer, db.ForeignKey('user.id')))
-friends = db.Table('user_friend', db.Column('user', db.Integer, db.ForeignKey('user.id')), db.Column('user', db.Integer, db.ForeignKey('user.id')))
+friendsRel = db.Table('friendsRel', db.Column('user', db.Integer, db.ForeignKey('user.id')), db.Column('friend', db.Integer, db.ForeignKey('user.id')))
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
@@ -9,7 +9,7 @@ class User(db.Model):
 	name = db.Column(db.String(32))
 	image_url = db.Column(db.String(60))
 	groups = db.relationship(Group, secondary=groups, backref=db.backref('group_user', lazy='dynamic'))
-	friends = db.relationship('User', secondary=friends, backref=db.backref('user_friend', lazy='dynamic'))
+	friends = db.relationship('User', secondary=friendsRel, primaryjoin=(friendsRel.c.user == id), secondaryjoin=(friendsRel.c.friend == id), backref=db.backref('friendsRel', lazy='dynamic'))
 
 	def __init__(self, username, name, image_url):
 		self.username = username
